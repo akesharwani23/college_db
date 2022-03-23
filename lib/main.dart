1493f4a03a1d53b06be5c96e244435320a292d63
Dhyan Singh <dhyansingh24042001@gmail.com>
2022-03-23 16:29:44 +0530

    start

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CollegeDB'),
+      ),
+      body: const Center(
+          child: Text(
+        '42',
+        style: TextStyle(fontSize: 42, color: Colors.grey),
+      )),
+    );
+  }
+}
