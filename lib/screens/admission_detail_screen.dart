248724d55b104ee0efa2af40890926bcf335a535
Dhyan Singh <dhyansingh24042001@gmail.com>
2022-04-30 17:47:33 +0530

    updated AdmissionDetailScreen to contain all fields

--- lib/screens/admission_detail_screen.dart
+++ lib/screens/admission_detail_screen.dart
@@ -11,6 +11,12 @@ class AdmissionDetailScreen extends StatelessWidget {
   static const routeName = '/admission-detail-screen';
   const AdmissionDetailScreen({Key? key}) : super(key: key);
 
+  static const Map<String, Color> _statusColor = {
+    'Confirmed': Colors.greenAccent,
+    'Provisional': Colors.yellowAccent,
+    'Registration': Colors.redAccent,
+  };
+
   @override
   Widget build(BuildContext context) {
     final AdmissionCandidate candidate =
@@ -57,28 +63,16 @@ class AdmissionDetailScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              if (candidate.status == 'Confirmed')
-                Container(
-                  child: Text(
-                    candidate.status,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  color: Colors.greenAccent,
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.all(8),
+              Container(
+                child: Text(
+                  candidate.status,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
-              if (candidate.status == 'Not Confirmed')
-                Container(
-                  child: Text(
-                    candidate.status,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  color: Colors.redAccent,
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.all(8),
-                ),
+                color: _statusColor[candidate.status],
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(8),
+              ),
               const Center(
                 child: Text(
                   'Personal Details',
@@ -86,19 +80,18 @@ class AdmissionDetailScreen extends StatelessWidget {
                 ),
               ),
               const Divider(),
-              textDetailElement('Name', candidate.name),
-              textDetailElement(
+              detailWidget('Name', candidate.name),
+              detailWidget(
                   'DOB', DateFormat("EEE, dd/MM/yyyy").format(candidate.dob)),
-              textDetailElement('Mobile Number', candidate.mobileNumber),
-              textDetailElement('Parent\'s Name', candidate.parentName),
-              textDetailElement(
+              detailWidget('Mobile Number', candidate.mobileNumber),
+              detailWidget('Parent\'s Name', candidate.parentName),
+              detailWidget(
                   'Parent\'s Mobile Number', candidate.parentMobileNumber),
-              textDetailElement('Category', candidate.category),
-              textDetailElement('Address', candidate.address),
-              textDetailElement(
+              detailWidget('Category', candidate.category),
+              detailWidget('Address', candidate.address),
+              detailWidget(
                   'Parent Mobile Number', candidate.parentMobileNumber),
-              textDetailElement(
-                  'Parent\'s Occupation', candidate.parentOccupation),
+              detailWidget('Parent\'s Occupation', candidate.parentOccupation),
               const SizedBox(
                 height: 20,
               ),
@@ -109,15 +102,14 @@ class AdmissionDetailScreen extends StatelessWidget {
                 ),
               ),
               const Divider(),
-              textDetailElement(
+              detailWidget(
                   'Previous Institute Name', candidate.previousInstituteName),
-              textDetailElement('Roll No Last Exam', candidate.rollNoLastExam),
-              textDetailElement('Appeared In Entrance Exam',
+              detailWidget('Roll No Last Exam', candidate.rollNoLastExam),
+              detailWidget('Appeared In Entrance Exam',
                   candidate.appearedInEntranceExam ? 'Yes' : 'No'),
-              textDetailElement(
-                  'Name Entrance Exam', candidate.nameEntranceExam),
-              textDetailElement(
-                  'Rank Entrance Exam', candidate.rankEntranceExam),
+              detailWidget('Name Entrance Exam', candidate.nameEntranceExam),
+              detailWidget('Rank Entrance Exam', candidate.rankEntranceExam),
+              detailWidget('Score Entrance Exam', candidate.scoreEntranceExam),
               const SizedBox(
                 height: 20,
               ),
@@ -130,20 +122,40 @@ class AdmissionDetailScreen extends StatelessWidget {
                 ),
               ),
               const Divider(),
-              textDetailElement('Graduation Type', candidate.courseType),
-              textDetailElement('Course', candidate.course),
-              textDetailElement('Branch', candidate.branch),
-              textDetailElement(
-                  'Fee/Sem', candidate.feeForSem.toStringAsFixed(2)),
-              textDetailElement('Fee Paid By Student',
+              detailWidget('Session', candidate.session),
+              detailWidget('Graduation Type', candidate.courseType),
+              detailWidget('Course', candidate.course),
+              detailWidget('Branch', candidate.branch),
+              detailWidget('Fee/Sem', candidate.feeForSem.toStringAsFixed(2)),
+              detailWidget('Fee Paid By Student',
                   candidate.paidByStudent.toStringAsFixed(2)),
-              // textDetailElement(, data)
+              detailWidget('Cancellation Charges',
+                  candidate.cancellationCharge.toStringAsFixed(2)),
+              const SizedBox(
+                height: 20,
+              ),
+              const Center(
+                child: Text(
+                  'Other Details',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              const Divider(),
+              detailWidget('Remark', candidate.remark),
+              detailWidget(
+                  'Admission Date',
+                  DateFormat("EEE, dd/MM/yyyy")
+                      .format(candidate.admissionDate)),
+              detailWidget('Admitted By', candidate.admittedBy),
+              detailWidget('Admission Incharge', candidate.admissionIncharge)
             ],
           ),
         ));
   }
 
-  Widget textDetailElement(String type, String data) {
+  Widget detailWidget(String type, String data) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 4, right: 8, left: 8),
       child: Text(
