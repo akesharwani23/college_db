a902e7ebcd666bae4ffb3f98681b1354bf8373b2
Dhyan Singh <dhyansingh24042001@gmail.com>
2022-05-08 14:08:50 +0530

    updated NotVerifiedScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/current_user.dart';
@@ -22,20 +23,43 @@ class HomeScreen extends StatelessWidget {
                 return const VerifiedHomeScreen();
               } else {
                 return Scaffold(
-                  appBar: AppBar(title: const Text('Not Verified')),
+                  appBar: AppBar(
+                    title: const Text('Not Verified'),
+                    backgroundColor: Colors.redAccent,
+                  ),
                   drawer: const AppDrawer(),
-                  body: Center(
-                      child: Container(
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.red)),
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'This Account Is Not Verified',
-                        style: TextStyle(color: Colors.red),
-                      ),
+                  body: Container(
+                    color: const Color.fromARGB(255, 246, 237, 237),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 39,
+                        ),
+                        const FaIcon(
+                          FontAwesomeIcons.ban,
+                          size: 66,
+                          color: Colors.redAccent,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Center(
+                            child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.red)),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'This Account Is Not Verified',
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        )),
+                      ],
                     ),
-                  )),
+                  ),
                 );
               }
             }
